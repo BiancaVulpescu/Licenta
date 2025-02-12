2e9610846f0c7bf9e021a123f66ca4e0bceaaff7
BiancaVulpescu <126167993+BiancaVulpescu@users.noreply.github.com>
2025-02-12 18:42:12 +0200

    nimic

--- DrivoCare2/app/src/main/java/com/example/drivocare/pages/LoginPage.kt
+++ DrivoCare2/app/src/main/java/com/example/drivocare/pages/LoginPage.kt
@@ -2,7 +2,6 @@ package com.example.drivocare.pages
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
