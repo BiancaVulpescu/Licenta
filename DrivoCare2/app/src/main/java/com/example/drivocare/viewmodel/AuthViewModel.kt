b77208fcb9b48aa370e65c63b60237895f939aa2
BiancaVulpescu <126167993+BiancaVulpescu@users.noreply.github.com>
2025-05-08 13:44:34 +0300

    change password and username finished

--- DrivoCare2/app/src/main/java/com/example/drivocare/viewmodel/AuthViewModel.kt
+++ DrivoCare2/app/src/main/java/com/example/drivocare/viewmodel/AuthViewModel.kt
@@ -1,5 +1,6 @@
 package com.example.drivocare.viewmodel
 
+import androidx.compose.runtime.mutableStateOf
 import androidx.lifecycle.ViewModel
 import com.example.drivocare.data.AuthState
 import com.example.drivocare.repositories.AuthRepository
@@ -7,6 +8,7 @@ import com.example.drivocare.repositories.AuthRepository
 class AuthViewModel : ViewModel() {
     private val repository = AuthRepository()
     val authState = repository.authState
+    val currentUsername = mutableStateOf("")
 
     fun login(email: String, password: String) {
         repository.login(email, password)
@@ -15,7 +17,17 @@ class AuthViewModel : ViewModel() {
     fun register(email: String, password: String, username: String) {
         repository.register(email, password, username)
     }
-
+    fun updateUsername(newUsername: String, onSuccess: () -> Unit, onError: (String) -> Unit) {
+        repository.updateUsername(newUsername, onSuccess, onError)
+    }
+    fun fetchCurrentUsername() {
+        repository.fetchCurrentUsername(
+            onSuccess = { currentUsername.value = it }
+        )
+    }
+    fun changePassword(currentPassword: String, newPassword: String, onSuccess: () -> Unit, onError: (String) -> Unit) {
+        repository.changePassword(currentPassword, newPassword, onSuccess, onError)
+    }
     fun logout() {
         repository.logout()
     }
